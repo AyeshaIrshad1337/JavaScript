d767c1a760b992134e2135251c88ebc231d51799
Ayesha Irshad <104616632+AyeshaIrshad1337@users.noreply.github.com>
2023-04-23 09:22:41 +0500

    Create String.js

---
+++ String.js
@@ -0,0 +1,24 @@
+// Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+// I am a "double quoted" string inside "double quotes". 
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+// Quoting Strings with Single 
+
+// Here badStr will throw an error.
+
+// In the goodStr above, you can use both quotes safely by using the backslash \ as an escape character.
+
+// Note: The backslash \ should not be confused with the forward slash /. They do not do the same thing.
+// Changed code
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// //Escape Sequences in Strings
+// Assign the following three lines of text into the single variable myStr using escape sequences.
+
+// FirstLine
+//     \SecondLine
+// ThirdLine
+// You will need to use escape sequences to insert special characters correctly. You will also need to follow the spacing as it looks above, with no spaces between escape sequences or words.
+
+// Note: The indentation for SecondLine is achieved with the tab escape character, not spaces.
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
